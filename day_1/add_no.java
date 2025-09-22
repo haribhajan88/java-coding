b243c6502f9a39ba2aaa7e3bcb545099c7233f68
haribhajan88 <haribhajank91@gmail.com>
2025-09-22 14:09:59 +0530

    [HariBhajan].add day_1 assignment

---
+++ day_1/add_no.java
@@ -0,0 +1,7 @@
+public class add_no {
+    public static void main(String[] args) {
+        int a = 10, b = 20, sum;
+        sum = a + b;
+        System.out.println("The sum is: " + sum);
+    }
+}
---
+++ day_1/circle_area.java
@@ -0,0 +1,7 @@
+public class circle_area {
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area is" + area);
+    }
+}
