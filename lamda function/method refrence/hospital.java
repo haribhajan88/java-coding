5910ca4a07a21c99322d87037b4b21219668e05b
haribhajan88 <haribhajank91@gmail.com>
2025-10-30 15:27:12 +0530

    lamda function
    [Haribhajan].add day 11

--- lamda function/method refrence/hospital.java
+++
@@ -1,5 +0,0 @@
-// Method References
-// 1. Hospital Patient ID Printing
-// ○ Scenario: Need to print all patient IDs from a list for admin verification.
-// ○ Task: Use method references instead of lambdas.
-
