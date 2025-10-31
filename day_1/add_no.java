0a2a13759a03ac5164f51dd108bf634aa2ced316
haribhajan88 <haribhajank91@gmail.com>
2025-10-31 13:28:55 +0530

    Delete day_1 directory

--- day_1/add_no.java
+++
@@ -1,7 +0,0 @@
-public class add_no {
-    public static void main(String[] args) {
-        int a = 10, b = 20, sum;
-        sum = a + b;
-        System.out.println("The sum is: " + sum);
-    }
-}
--- day_1/circle_area.java
+++
@@ -1,7 +0,0 @@
-public class circle_area {
-    public static void main(String[] args) {
-        double radius = 7.5;
-        double area = Math.PI * radius * radius;
-        System.out.println("The area is" + area);
-    }
-}
